a588262c0d5f115ef653e11a3615b3da242b741f
31355587 <31355587@099242-P31S204.macklabs.br>
2016-09-29 11:01:16 -0300

    teste Junit

---
+++ Source Code Inspection/test/br/calebe/ticketmachine/test/TesteTicketMachine.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.calebe.ticketmachine.test;
+
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 31355587
+ */
+public class TesteTicketMachine {
+    
+    public TesteTicketMachine() {
+        
+       
+    }
+     
+
+   
+     @Test
+     public void hello() {
+      int x=0;
+        int y=2;
+     Assert.assertEquals(2, y);
+     
+     
+     }
+}
