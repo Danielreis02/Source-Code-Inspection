55166558a3e3675e4bebdb788f96dbe8caec9d8c
Daniel Reis <Daniel Reis@Daniel>
2016-10-02 22:42:18 -0300

    Teste

---
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TrcoIterator.java
@@ -0,0 +1,48 @@
+package br.calebe.ticketmachine.core;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Daniel Reis
+ */
+class TrocoIterator implements Iterator<PapelMoeda> {
+    
+
+        private Troco troco;
+        private PapelMoeda current;
+        private int indexCurrent;
+
+        public TrocoIterator(Troco troco) {
+            this.troco = troco;
+            indexCurrent = 0;
+            current = troco.getPapeisMoeda().get(0);
+        }
+
+        public void first(){
+            indexCurrent = 0;
+            current = troco.getPapeisMoeda().get(0);           
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return troco.getPapeisMoeda().get(indexCurrent+1)!=null;
+        }
+
+        @Override
+        public PapelMoeda next() {
+            indexCurrent++;
+            current = troco.getPapeisMoeda().get(indexCurrent);
+            return current;
+        }
+
+        @Override
+        public void remove() {
+            troco.getPapeisMoeda().remove(current);
+            if(indexCurrent==troco.getPapeisMoeda().size()){
+                indexCurrent = indexCurrent-1;
+                current = troco.getPapeisMoeda().get(indexCurrent);
+            }else{
+                current = troco.getPapeisMoeda().get(indexCurrent);
+            }
+        }
+    }
--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -1,5 +1,6 @@
 package br.calebe.ticketmachine.core;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
@@ -8,95 +9,44 @@ import java.util.Iterator;
  */
 class Troco {
 
-    protected PapelMoeda[] papeisMoeda;
+    private ArrayList<PapelMoeda> papeisMoeda;
 
     public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
-            //Tipo de Defeito: Desempenho 
-            //Descriçao: o loop esta sendo usado sem necessidade.
-            //Descriçao: Aa condiçao dentro do loop so vai parar de 
-            //executar se o valor for multiplo de 100 caso entrara em loop.  
+        this.papeisMoeda = new ArrayList(6);
+        
+        int quant_notas = valor / 100;
+        valor %= 100;
+        papeisMoeda.add(5, new PapelMoeda(100, quant_notas));
+        
+        quant_notas = valor / 50;
+        valor %= 50;        
+        papeisMoeda.add(4, new PapelMoeda(50, quant_notas));
+        
+        quant_notas = valor / 20;
+        valor %= 20;        
+        papeisMoeda.add(3, new PapelMoeda(20, quant_notas));
+        
+        quant_notas = valor / 10;
+        valor %= 10;       
+        papeisMoeda.add(2, new PapelMoeda(10, quant_notas));
+        
+        if (valor != 6 && valor != 8) {
+            quant_notas = valor / 5;
+            valor %= 5;
+            papeisMoeda.add(1, new PapelMoeda(5, quant_notas));
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-            //Tipo de Defeito:Desempenho
-            // Descriçao: o loop esta sendo usado sem necessidade
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+        
+        quant_notas = valor / 2;
+        valor %= 2;
+        papeisMoeda.add(0, new PapelMoeda(2, quant_notas));
     }
+
+    public ArrayList<PapelMoeda> getPapeisMoeda() {
+        return papeisMoeda;
+    }
+    
 
     public Iterator<PapelMoeda> getIterator() {
         return new TrocoIterator(this);
-    }
-
-    class TrocoIterator implements Iterator<PapelMoeda> {
-
-        protected Troco troco;
-
-        public TrocoIterator(Troco troco) {
-            this.troco = troco;
-        }
-
-        @Override
-        public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
-                
-                // Tipo de Defeito: Controle
-                //Descrição: o for esta com sus condiçoes erradas causando um 
-                //loop eterno onde a variavel (i) sempre será maior que zero 
-                if (troco.papeisMoeda[i] != null) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public PapelMoeda next() {
-            PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
-                    troco.papeisMoeda[i] = null;
-                }
-                //Tipo de Defeito: controle
-                // Descriçao:O if começa nesse caso ja ultima possição vetor (papeisMoeda)
-                //quando o (i) for incerrado vai gerar um erro de indice inexistente
-                // no vetor (papeisMoeda).
-            }
-            return ret;
-        }
-
-        @Override
-        public void remove() {
-            //tipo de Defeito : Excesso 
-            //Descrição: o metodo esta chamando outro metdo que esta em um loop infinito 
-            next();
-        }
     }
 }
